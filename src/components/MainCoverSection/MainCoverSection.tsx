fc2d05d2243709bbee0f4b2d5e671fb349ebacea
vertexofvortex <tbm92ru@gmail.com>
2023-10-02 14:51:52 +0300

    ✨ Секция-обложка на главной

---
+++ src/components/MainCoverSection/MainCoverSection.tsx
@@ -0,0 +1,26 @@
+import Section from "../Section/Section";
+import s from "./MainCoverSection.module.scss";
+
+export const MainCoverSection = () => {
+  return (
+    <Section className={s.mainCoverSection}>
+      <div className={s.text}>
+        <h1 className="margin-bottom">
+          Классика
+          <br /> – и ничего лишнего
+        </h1>
+        <p>
+          mine! — новый игровой сервер Minecraft в стиле классического
+          выживания. Мы соединили воедино только самое важное, чтобы получилось
+          место, где вы и ваши друзья смогут проводить время часами и не жалеть
+          об этом. Включайтесь прямо сейчас!
+        </p>
+      </div>
+      <div className={s.picture}>
+        <img src="/images/chelik_1.png" alt="" />
+      </div>
+    </Section>
+  );
+};
+
+export default MainCoverSection;
--- src/routes/Main/Main.tsx
+++ src/routes/Main/Main.tsx
@@ -1,30 +1,10 @@
-import Section from "../../components/Section/Section";
+import MainCoverSection from "../../components/MainCoverSection/MainCoverSection";
 import s from "./Main.module.scss";
 
 export const Main = () => {
   return (
     <div className={`route main ${s.route}`}>
-      <Section>
-        <h1>Классика и ничего лишнего</h1>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-          sequi facere laborum ut possimus voluptates natus obcaecati. Deserunt
-          impedit blanditiis modi doloribus explicabo, officia, quod corporis
-          nihil voluptatum hic laborum.
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-          sequi facere laborum ut possimus voluptates natus obcaecati. Deserunt
-          impedit blanditiis modi doloribus explicabo, officia, quod corporis
-          nihil voluptatum hic laborum.
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-          sequi facere laborum ut possimus voluptates natus obcaecati. Deserunt
-          impedit blanditiis modi doloribus explicabo, officia, quod corporis
-          nihil voluptatum hic laborum.
-        </p>
-      </Section>
+      <MainCoverSection />
     </div>
   );
 };
