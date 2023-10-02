7802d3f1b904b54bfda9d28eaa389c8776a17563
vertexofvortex <tbm92ru@gmail.com>
2023-10-02 14:05:59 +0300

    ✨ Хедер

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,39 @@
 import { Link } from "react-router-dom";
 import s from "./Header.module.scss";
+import { useEffect, useState } from "react";
 
 export const Header = () => {
+  const [isCompact, setIsCompact] = useState<boolean>(false);
+
+  const handleWindowScroll = () => {
+    if (window.scrollY > 50) {
+      setIsCompact(true);
+    } else {
+      setIsCompact(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleWindowScroll);
+
+    return () => window.removeEventListener("scroll", handleWindowScroll);
+  }, []);
+
   return (
-    <header className={s.header}>
-      <div className="logo">mine! [open beta] | Minecraft 1.20</div>
-      <div className="links">
-        <Link to={"/"}>main</Link> | <Link to={"/shop"}>shop</Link>
+    <header className={`${s.header} ${isCompact ? s.compact : ""}`}>
+      <div className={s.wrapper}>
+        <div className={s.logo}>
+          <h3>mine!</h3>
+          <div>Minecraft 1.20</div>
+        </div>
+        <div className={s.links}>
+          <Link to={"/"}>Главная</Link>
+          <Link to={"/shop"}>FAQ</Link>
+          <Link to={"/shop"}>Магазин</Link>
+          <Link to={"/shop"}>ЛК</Link>
+          <Link to={"/shop"}>Discord</Link>
+          <button>Играть</button>
+        </div>
       </div>
     </header>
   );
--- src/routes/Main/Main.tsx
+++ src/routes/Main/Main.tsx
@@ -6,6 +6,24 @@ export const Main = () => {
     <div className={`route main ${s.route}`}>
       <Section>
         <h1>Классика и ничего лишнего</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+          sequi facere laborum ut possimus voluptates natus obcaecati. Deserunt
+          impedit blanditiis modi doloribus explicabo, officia, quod corporis
+          nihil voluptatum hic laborum.
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+          sequi facere laborum ut possimus voluptates natus obcaecati. Deserunt
+          impedit blanditiis modi doloribus explicabo, officia, quod corporis
+          nihil voluptatum hic laborum.
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+          sequi facere laborum ut possimus voluptates natus obcaecati. Deserunt
+          impedit blanditiis modi doloribus explicabo, officia, quod corporis
+          nihil voluptatum hic laborum.
+        </p>
       </Section>
     </div>
   );
